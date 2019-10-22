4d3681d3268925ba738ab4ff3d05f5e570f4823d
xiaoer <xiaoer@example.com>
2019-10-22 15:32:58 +0800

    path还原

--- data/Path.ts
+++ data/Path.ts
@@ -8,19 +8,12 @@ module gamebuyu.data {
 		static ui_buyu: string = "buyu_ui/game_ui/buyu/";
 		static atlas_game_ui: string = "buyu_res/atlas/buyu_ui/game_ui/";
 		
+		static custom_atlas_fish: string = 'custom_atlas/scene/fish/';
+
+		static custom_atlas_yw: string = 'custom_atlas/scene/yw/';
 		static scene_single: string = 'scene/single/';
 		static fish: string = 'scene/fish/';
 		static scene_bullet: string = 'scene/bullet/';
 		static scene_yw: string = 'scene/yw/';
-
-		static get custom_atlas():string {
-			return "custom_atlas/" + WebConfig.platform + "/";
-		}
-		static get custom_atlas_fish():string {
-			return Path.custom_atlas + "scene/fish/";
-		}
-		static get custom_atlas_yw():string {
-			return Path.custom_atlas + "scene/yw/";
-		}
 	}
 }
